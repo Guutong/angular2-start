7f979bf750c7f9b84aed0b3d0dfda276411de79b
Pornmongkon Pongsai <pornmongkonp@riverpark.co.th>
2017-03-16 00:09:10 +0700

    Add Hero service in memory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TongComponent } from './tong.component';
+import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,8 +19,10 @@ import { TongComponent } from './tong.component';
     BrowserModule,
     FormsModule,
     HttpModule
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+  providers: [
+    HeroService
   ],
-  providers: [],
   bootstrap: [TongComponent]
 })
 export class AppModule { }
